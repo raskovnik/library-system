e03e16ef4082c8549c640094094a83b083a467bb
Anthony Mwangi <4lch3my.3ck0@gmail.com>
2023-03-21 22:05:47 +0300

    approve and reject books (#33)

--- pages/viewrequest.php
+++ pages/viewrequest.php
@@ -18,18 +18,52 @@
             $query = mysqli_query($conn, $sql);
             $result = mysqli_fetch_assoc($query);
         ?>
-        <form method="POST" action="viewrequest.php">
+        <form method="POST">
             <label>Registration Number</label>
             <input type="text" disabled="true"  name="reg" id="reg" <?php echo 'value='.$result["reg"].''?>>
             <label>ISBN</label>
-            <input type="text" disabled="true"  name="isbn" id="isbn" <?php echo 'value='.$result["reg"].''?>>
+            <input type="text" disabled="true"  name="isbn" id="isbn" <?php echo 'value='.$result["isbn"].''?>>
             <label>Borrow Date</label>
             <input type="text" disabled="true" name="b_date" id="b_date" <?php echo 'value='.$result["request_date"].''?>>
             <label>Return Date</label>
-            <input type="text" disabled="true" name="r_date" id="r_date" <?php echo 'value='.date("Y/m/d", strtotime($result["request_date"]."+ 14 days")).''?>>
+            <input type="text" disabled="true" name="r_date" id="r_date" <?php echo 'value='.date("Y/m/d", strtotime($result["request_date"]."+ 14 days")).'';?>>
             <input type="submit" name="approve" id="approve" value="Approve" class="btn" style="width: 25%; background-color: green;">
             <input type="submit" name="reject" id="reject" value="Reject" class="btn" style="width: 25%; background-color: red;">
         </form>
         <a href="dashboard.php"><button>Back to Dashboard</a>
+
+        <?php
+            if (array_key_exists("approve", $_POST)) {
+                $borrow_date = $result["request_date"];
+                $return_date = date("Y/m/d", strtotime($result["request_date"]."+ 14 days"));
+                $isbn = $result["isbn"];
+                $sql = "INSERT INTO `borrow`(`reg`,`isbn`,`borrow`,`return_date`) VALUES('$reg','$isbn','$borrow_date', '$return_date')";
+                // $sql = "INSERT INTO `borrow`(`reg`, `isbn`, `borrow`, `return_date`) VALUES('1234', '123455434', '2023/3/21', '2023/4/4')";
+                $res = mysqli_query($conn, $sql);
+                if ($res) {
+                    $sql = "DELETE FROM `requests`  WHERE `reg`=$reg";
+                    $del = mysqli_query($conn, $sql);
+                    if ($del) {
+                        $quantity = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM `books` WHERE `isbn`=$isbn"))["Quantity"];
+                        $q = $quantity - 1;
+                        $upd = mysqli_query($conn, "UPDATE `books` SET `Quantity`=$q WHERE `isbn`=$isbn");
+                        if ($upd) {
+                            echo "Book approved Successfully. To be returned on '.$return_date.'";
+                            header("location:requests.php");
+                        }
+                    }
+                } else {
+                    echo "Error approving book borrow";
+                }
+            }
+
+            if (array_key_exists("reject", $_POST)) {
+                $sql = "DELETE FROM `requests`  WHERE `reg`=$reg";
+                $del = mysqli_query($conn, $sql);
+                if ($del) {
+                    header("location:requests.php");
+                }
+            }
+        ?>
     </body>
 </html>
