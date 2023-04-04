ad27ce8d65b52baf510caf5a7f38678b17dc8a7f
Anthony K. Mwangi <68157384+raskovnik@users.noreply.github.com>
2023-04-04 23:06:48 +0300

    add lost books badge

--- pages/navbar.php
+++ pages/navbar.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-wZsYN0TcWbpg8xGy+NTtTzGSLAAsV7/Z8K0TBuV7oLeQJ4fi4ddX4E4zKl+TGPTBwDrM13VpGWjCxuI7VGlGGQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <div class="container">
@@ -23,13 +27,27 @@
                         <li>
                             <div class="dropdown">
                                 <?php
+                                    include "../scripts/connect.php";
                                     session_start();
                                     if (isset($_SESSION["user"])) {
-                                        echo '<a href="#">Account</a>';
-                                        echo '<div class="dropdown-content" style="text-align: left;">';
-                                            // echo '<a href="#">Borrow History</a>';
-                                            echo '<a href="invoice.php?reg='.$_SESSION["user"].'">Lost and Overdue Books</a>';
-                                            echo '<a href="logout.php">Log Out</a>';
+                                        $user = $_SESSION["user"];
+                                        $pending = mysqli_num_rows(
+                                            mysqli_query($conn, "SELECT * FROM `borrow` WHERE `reg`=$user")) + 
+                                            mysqli_num_rows(mysqli_query($conn, "SELECT * FROM `lost` WHERE `reg`=$user"));
+                                        if ($pending != 0) {
+                                            echo '<a href="#">Account <span class="badge badge-danger"><i class="bi bi-exclamation-circle" style="font-size: 1.3rem;"></i></span></a>';
+                                            echo '<div class="dropdown-content" style="text-align: left;">';
+                                                // echo '<a href="#">Borrow History</a>';
+                                                echo '<a href="invoice.php?reg='.$_SESSION["user"].'">Lost and Overdue Books</a>';
+                                                echo '<a href="logout.php">Log Out</a>';
+                                            echo '</div>';
+                                        } else {
+                                            echo '<a href="#">Account</a>';
+                                            echo '<div class="dropdown-content" style="text-align: left;">';
+                                                // echo '<a href="#">Borrow History</a>';
+                                                echo '<a href="invoice.php?reg='.$_SESSION["user"].'">Lost and Overdue Books</a>';
+                                                echo '<a href="logout.php">Log Out</a>';
+                                        }
                                         echo '</div>';
                                     } else {
                                         echo '<a href="login.php">Account</a>'; 
